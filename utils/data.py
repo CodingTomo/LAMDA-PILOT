064b39df316b171c9ac080c6ce819f56de732fa1
CodingTomo <tomaso.trinci90@gmail.com>
2024-06-13 15:08:25 +0200

    add  domainnet

--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from torchvision import datasets, transforms
 from utils.toolkit import split_images_labels
+import os
 
 
 class iData(object):
@@ -225,14 +226,75 @@ class iImageNetR(iData):
 
     def download_data(self):
         # assert 0, "You should specify the folder of your dataset"
-        train_dir = "/media/data-turing/CL-Dataset/imagenet-r/train/"
-        test_dir = "/media/data-turing/CL-Dataset/imagenet-r/test/"
+        train_dir = "/media/data-lie/dataset/imagenet-r/train"
+        test_dir = "/media/data-lie/dataset/imagenet-r/test"
 
         train_dset = datasets.ImageFolder(train_dir)
         test_dset = datasets.ImageFolder(test_dir)
 
         self.train_data, self.train_targets = split_images_labels(train_dset.imgs)
         self.test_data, self.test_targets = split_images_labels(test_dset.imgs)
+
+
+class iDomainNet(iData):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.use_path = True
+
+        if args["model_name"] == "coda_prompt":
+            self.train_trsf = build_transform_coda_prompt(True, args)
+            self.test_trsf = build_transform_coda_prompt(False, args)
+        else:
+            self.train_trsf = build_transform(True, args)
+            self.test_trsf = build_transform(False, args)
+        self.common_trsf = [
+            # transforms.ToTensor(),
+        ]
+
+        self.class_order = np.arange(600).tolist()
+
+    def download_data(self):
+        # assert 0, "You should specify the folder of your dataset"
+        train_test_dir = "/Scratch/Magistri/cl_data"
+
+        self.train_data, self.train_targets, self.test_data, self.test_targets = self.get_train_val_images_domainnet(train_test_dir)
+
+
+    def get_train_val_images_domainnet(self, data_path):
+        train_annotations = open(os.path.join(data_path, "DomainNet",  "cs_train_6.txt"), "r")
+        test_annotations = open(os.path.join(data_path, "DomainNet",  "cs_test_6.txt"), "r")
+
+        train_lines = train_annotations.readlines()
+        test_lines = test_annotations.readlines() 
+
+        train_images = []
+        train_labels = []
+        for item in train_lines:
+            splitted_item = item.rstrip()
+            splitted_item  = splitted_item.split(" ")
+            image_path = "/".join([splitted_item[0].split("/")[0],splitted_item[0].split("/")[1], splitted_item[0].split("/")[2]])
+            image_path = os.path.join(data_path, 'DomainNet', image_path)
+            train_images.append(image_path)
+            train_labels.append(int(splitted_item[1]))
+        
+        train_images = np.array(train_images)
+        train_targets = np.array(train_labels)
+        
+        test_images = []
+        test_labels = []
+        for item in test_lines:
+            splitted_item = item.rstrip()
+            splitted_item  = splitted_item.split(" ")
+            image_path = "/".join([splitted_item[0].split("/")[0], splitted_item[0].split("/")[1], splitted_item[0].split("/")[2]])
+            image_path = os.path.join(data_path, 'DomainNet', image_path)
+            test_images.append(image_path)
+            test_labels.append(int(splitted_item[1]))
+        
+        test_images = np.array(test_images)
+        test_targets = np.array(test_labels)
+
+        return train_images, train_targets, test_images, test_targets
 
 
 class iImageNetA(iData):
