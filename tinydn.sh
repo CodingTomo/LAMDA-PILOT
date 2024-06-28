a99a86de359a570bacc4c25deca77f8861ab2962
CodingTomo <tomaso.trinci90@gmail.com>
2024-06-28 10:45:49 +0200

    add sh for tinydn

---
+++ tinydn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/simplecil_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/ranpac_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/ease_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/finetune_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/l2p_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/jointincremental_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/dualprompt_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/coda_prompt_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/icarl_tinydn.json
+
+python -u main.py --config /home/trinci/git/LAMDA-PILOT/exps_tiny_domainnet/memo_tinydn.json
+
